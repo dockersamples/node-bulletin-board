25c672e84c426bcf838a6f0f904fadc4152954b3
Vlad Kulakouski <kula4ud3e@gmail.com>
2020-12-11 18:33:14 +0300

    Update api.js

--- bulletin-board-app/backend/api.js
+++ bulletin-board-app/backend/api.js
@@ -3,7 +3,7 @@ var events = require('./events.js');
 exports.events = function (req, res) {
   res.json(events);
 };
-
+#
 exports.event = function (req, res) {
   res.json(events[req.param.eventId]);
 };
